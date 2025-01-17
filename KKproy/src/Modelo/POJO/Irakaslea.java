2c6bb9e14dd523a6202893ad2b3ae72b7dc6399b
infopalacios <infopalacios@HZ327439a.elorrieta.int>
2025-01-17 10:23:57 +0100

    getters eta setters de irakaslea

--- KKproy/src/Modelo/POJO/Irakaslea.java
+++ KKproy/src/Modelo/POJO/Irakaslea.java
@@ -4,6 +4,24 @@ public class Irakaslea {
 	
 	private int id_irakas;
 	private String izena;
+	public Irakaslea(int id_irakas, String izena) {
+		
+		this.id_irakas = id_irakas;
+		this.izena = izena;
+	}
+	public int getId_irakas() {
+		return id_irakas;
+	}
+	public void setId_irakas(int id_irakas) {
+		this.id_irakas = id_irakas;
+	}
+	public String getIzena() {
+		return izena;
+	}
+	public void setIzena(String izena) {
+		this.izena = izena;
+	}
+	
 	
 
 }
